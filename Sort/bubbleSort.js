9aa45b98cf4af55fa6856c470553240e53b292d6
Joed Machado <joedmg58@gmail.com>
2018-09-07 06:34:10 -0400

    bubbleSort function created

--- Sort/bubbleSort.js
+++ Sort/bubbleSort.js
@@ -4,3 +4,12 @@ function swap( array, i, j ){
     array[j] = tmp;
 }
 
+function bubbleSort( array ) {
+    for (var i = 0; i < array.length; i++){
+        for (var j = 1; j < array.length; j++ ){
+            if (array[j-1] > array[j]){
+                swap(array, j-1, j);
+            }
+        }
+    }
+}
